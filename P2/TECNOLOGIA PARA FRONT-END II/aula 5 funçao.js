8500e8404df341ff0f3278d0c304baa62e4e5e01
MuriloKaspar <mkdn81@hotmail.com>
2023-03-23 20:18:41 -0300

    atualização

---
+++ P2/TECNOLOGIA PARA FRONT-END II/aula 5 funçao.js
@@ -0,0 +1,19 @@
+function somar(numero1, numero2){
+    var total = numero1+numero2;
+    return total;
+}
+
+var resultado = somar (1,5);
+console.log(resultado);
+
+var multiplicar = function(n1,n2){
+    return (n1*n2);
+}
+
+multiplicar(2,6);
+
+//usando "hoisting" a declaração da função é "enviada" para o topo no pre-processamento
+//function somar(numero1,numero2){
+//    var total = numero1+numero2;
+//    return total;
+//}
